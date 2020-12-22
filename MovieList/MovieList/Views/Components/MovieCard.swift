fbd19f36aedca5dc18f397603f92a14d21ec65f8
Fernando Martin Garcia Del Angel <martingarciadelangel@me.com>
2020-12-21 19:11:14 -0600

    Created Simple Movie Card
    Added a simple card view, still needs some refactoring

---
+++ MovieList/MovieList/Views/Components/MovieCard.swift
@@ -0,0 +1,20 @@
+//
+//  MovieCard.swift
+//  MovieList
+//
+//  Created by Fernando Martin Garcia Del Angel on 21/12/20.
+//
+
+import SwiftUI
+
+struct MovieCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MovieCard_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieCard()
+    }
+}
