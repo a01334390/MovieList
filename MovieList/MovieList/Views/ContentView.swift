0546d7a9101da099b4882baaa05bcf2aac2efaf0
Fernando Martin Garcia Del Angel <martingarciadelangel@me.com>
2020-12-21 19:47:43 -0600

    Movie List is Ready
    Added Movie List

--- MovieList/MovieList/Views/ContentView.swift
+++ MovieList/MovieList/Views/ContentView.swift
@@ -12,24 +12,31 @@ struct ContentView: View {
     @State private var searchText : String = ""
     var hapticImpact = UIImpactFeedbackGenerator(style: .light)
     var body: some View {
-        VStack {
-            SearchBar(text: $searchText, placeholder: "Search for Movies...")
-            Spacer()
-            List {
-                ForEach(self.movieFeed.filter {
-                    self.searchText.isEmpty ? true : $0.title?.lowercased().contains(self.searchText.lowercased()) ?? false
-                }, id: \.self) { movie in
-                    Button(action: {
-                        self.hapticImpact.impactOccurred()
-                    }, label: {
-                        MovieCard(movie: movie)
-                            .onAppear {
-                                self.movieFeed.loadMoreMovies(currentItem: movie)
+        NavigationView {
+            VStack {
+                SearchBar(text: $searchText, placeholder: "Search for Movies...")
+                Spacer()
+                List {
+                    ForEach(self.movieFeed.filter {
+                        self.searchText.isEmpty ? true : $0.title?.lowercased().contains(self.searchText.lowercased()) ?? false
+                    }, id: \.self) { movie in
+                        ZStack {
+                            HStack {
+                                Spacer()
+                                MovieCard(movie: movie)
+                                    .onAppear {
+                                        self.movieFeed.loadMoreMovies(currentItem: movie)
+                                    }
+                                Spacer()
                             }
-                    })
-                    .buttonStyle(PlainButtonStyle())
+                            NavigationLink(destination: MovieDetail()) {
+                                EmptyView()
+                            }.buttonStyle(PlainButtonStyle())
+                        }
+                    }
                 }
             }
+            .navigationTitle("Movies")
         }
     }
 }
