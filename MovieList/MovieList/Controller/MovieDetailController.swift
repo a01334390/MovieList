d1416210374b36873f997a4451e143d694e4d126
Fernando Martin Garcia Del Angel <martingarciadelangel@me.com>
2020-12-21 19:58:19 -0600

    Added Movie Detail Model

---
+++ MovieList/MovieList/Controller/MovieDetailController.swift
@@ -0,0 +1,12 @@
+//
+//  MovieDetailController.swift
+//  MovieList
+//
+//  Created by Fernando Martin Garcia Del Angel on 21/12/20.
+//
+
+import Foundation
+
+class MovieDetailController: ObservableObject {
+    typealias Element = <#type expression#>
+}
--- MovieList/MovieList/Views/MovieDetail.swift
+++ MovieList/MovieList/Views/MovieDetail.swift
@@ -6,10 +6,28 @@
 //
 
 import SwiftUI
+import SDWebImageSwiftUI
 
 struct MovieDetail: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 2, content: {
+            Text("Guason")
+                .font(.largeTitle)
+            Text("Duracion")
+                .font(.headline)
+            Text("122 mins")
+            Text("Fecha de Estreno")
+                .font(.headline)
+            Text("2 octubre 2019")
+            Text("Calificacion")
+                .font(.headline)
+            Text("8.5")
+            Text("Generos")
+                .font(.headline)
+            Text("Crimen, Suspense, Drama")
+            Text("Descripcion")
+                .font(.headline)
+        })
     }
 }
 
